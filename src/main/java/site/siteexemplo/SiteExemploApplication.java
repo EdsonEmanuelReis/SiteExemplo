7de23eb016986f1b50dcbe86424b7fa6d015fb49
EdsonEmanuelReis <edemanuelreis.dev@gmail.com>
2026-08-23 02:51:11 -0300

    Primeiro commit do projeto

---
+++ src/main/java/site/siteexemplo/SiteExemploApplication.java
@@ -0,0 +1,13 @@
+package site.siteexemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteExemploApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiteExemploApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/siteexemplo/SiteExemploApplicationTests.java
@@ -0,0 +1,13 @@
+package site.siteexemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteExemploApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
